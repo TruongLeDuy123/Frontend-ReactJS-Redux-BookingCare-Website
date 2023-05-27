4ef06c1e21ebdf62eb1fbd53486cb734b3686453
truongle <20522083@gm.uit.edu.vn>
2023-05-27 15:47:33 +0700

    update writing APIs CRUD

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,20 +1,96 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManage.scss'
+import { getAllUsers } from '../../services/userService';
+import { Fragment } from 'react';
+import ModalUser from './ModalUser';
+
 class UserManage extends Component {
 
-    state = {
-
+    constructor(props) {
+        super(props)
+        this.state = {
+            arrUsers: [],
+            isOpenModalUser: false
+        }
     }
 
-    componentDidMount() {
-
+    /** life cycle
+     *  Run component:
+     * 1. Run construct -> init state
+     * 2. Did mount(set state)
+     * 3. Render
+     * 
+     *  */
+    async componentDidMount() {
+        let response = await getAllUsers('ALL')
+        if (response && response.errCode === 0) {
+            this.setState({
+                arrUsers: response.users
+            }, () => {
+                // console.log("check state user", this.state.arrUsers);
+            })
+        }
+        // console.log("get user from nodejs: ", response);
     }
 
+    handleAddNewUser = () => {
+        this.setState({
+            isOpenModalUser: !this.state.isOpenModalUser
+        })
+    }
+
+    toggleUserModal = () => {
+        this.setState({
+            isOpenModalUser: !this.state.isOpenModalUser
+        })
+    }
 
     render() {
+        // console.log('check render ', this.state);
+        let arrUsers = this.state.arrUsers
         return (
-            <div className="text-center">Manage users</div>
+            <div className="users-container">
+
+                <ModalUser 
+                    isOpen={this.state.isOpenModalUser}
+                    test = {'abc'}
+                    toggleFromParent={() => this.toggleUserModal()}
+                />
+
+                <div className="title text-center">Manage users with truong</div>
+                <div className='mx-1'>
+                    <button className='btn btn-primary px-3' onClick={() => this.handleAddNewUser()}><i className='fas fa-plus'></i> Add new users</button>
+                </div>
+                <div className='users-table mt-3 mx-1'>
+                    <table id="customers">
+                        <tr>
+                            <th>Email</th>
+                            <th>First name</th>
+                            <th>Last name</th>
+                            <th>Address</th>
+                            <th>Actions</th>
+                        </tr>
+                        {
+                            arrUsers && arrUsers.map((item, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td>{item.email}</td>
+                                        <td>{item.firstName}</td>
+                                        <td>{item.lastName}</td>
+                                        <td>{item.address}</td>
+                                        <td>
+                                            <button className='btn-edit'><i className='fas fa-pencil-alt'></i></button>
+                                            <button className='btn-delete'><i className='fas fa-trash'></i></button>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </table>
+                </div>
+            </div>
         );
     }
 
